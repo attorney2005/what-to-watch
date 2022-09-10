29601873da00368d80541dc8d7355db1aa3469f9
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-09-10 08:17:55 +0300

    Начал делать add-review

--- project/src/components/add-review/add-review.tsx
+++ project/src/components/add-review/add-review.tsx
@@ -1,76 +1,56 @@
-// import {films} from "../../mocks/films";
-// import {Films} from "../../types/films";
-//
-// const AddReviewProps {
-//   films = Films
-// }
-//
-// function AddReview(props:AddReviewProps): JSX.Element {
-//   const {films} = props;
-//   const {title, rating, id, src} = films;
-//   return (
-//     <section className="film-card film-card--full">
-//       <div className="film-card__header">
-//         <div className="film-card__bg">
-//           <img src={films.src} alt="The Grand Budapest Hotel"/>
-//         </div>
-//
-//         <h1 className="visually-hidden">WTW</h1>
-//
-//         <div className="film-card__poster film-card__poster--small">
-//           <img src={films.src} alt="The Grand Budapest Hotel poster" width="218"
-//                height="327"/>
-//         </div>
-//       </div>
-//
-//       <div className="add-review">
-//         <form action="#" className="add-review__form">
-//           <div className="rating">
-//             <div className="rating__stars">
-//               <input className="rating__input" id="star-10" type="radio" name="rating" value="10"/>
-//               <label className="rating__label" htmlFor="star-10">Rating 10</label>
-//
-//               <input className="rating__input" id="star-9" type="radio" name="rating" value="9"/>
-//               <label className="rating__label" htmlFor="star-9">Rating 9</label>
-//
-//               <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked/>
-//               <label className="rating__label" htmlFor="star-8">Rating 8</label>
-//
-//               <input className="rating__input" id="star-7" type="radio" name="rating" value="7"/>
-//               <label className="rating__label" htmlFor="star-7">Rating 7</label>
-//
-//               <input className="rating__input" id="star-6" type="radio" name="rating" value="6"/>
-//               <label className="rating__label" htmlFor="star-6">Rating 6</label>
-//
-//               <input className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
-//               <label className="rating__label" htmlFor="star-5">Rating 5</label>
-//
-//               <input className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
-//               <label className="rating__label" htmlFor="star-4">Rating 4</label>
-//
-//               <input className="rating__input" id="star-3" type="radio" name="rating" value="3"/>
-//               <label className="rating__label" htmlFor="star-3">Rating 3</label>
-//
-//               <input className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
-//               <label className="rating__label" htmlFor="star-2">Rating 2</label>
-//
-//               <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-//               <label className="rating__label" htmlFor="star-1">Rating 1</label>
-//             </div>
-//           </div>
-//
-//           <div className="add-review__text">
-//         <textarea className="add-review__textarea" name="review-text" id="review-text"
-//                   placeholder="Review text"></textarea>
-//             <div className="add-review__submit">
-//               <button className="add-review__btn" type="submit">Post</button>
-//             </div>
-//
-//           </div>
-//         </form>
-//       </div>
-//     </section>
-//   )
-// }
-//
-// export default AddReview
+import {films} from "../../mocks/films";
+import {Films} from "../../types/films";
+
+type AddReviewProps = {
+  films: Films
+}
+
+function AddReview(props:AddReviewProps): JSX.Element {
+  // const {films} = props;
+  // const {title, rating, id, src} = films;
+  return (
+    <section className="film-card film-card--full">
+      <div className="film-card__header">
+        <div className="film-card__bg">
+          <img src={films.src} alt="The Grand Budapest Hotel"/>
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <div className="film-card__poster film-card__poster--small">
+          <img src={films.src} alt="The Grand Budapest Hotel poster" width="218"
+               height="327"/>
+        </div>
+      </div>
+
+      <div className="add-review">
+        <form action="#" className="add-review__form">
+          <div className="rating">
+            <div className="rating__stars">
+              <input
+                className="rating__input"
+                id={`star-${films.rating}`}
+                type="radio"
+                name="rating"
+                value={films.rating}
+                // disabled={isRadioDisabled}
+              />
+              <label className="rating__label" htmlFor={`star-${films.rating}`}>Rating {films.rating}</label>
+            </div>
+          </div>
+
+          <div className="add-review__text">
+        <textarea className="add-review__textarea" name="review-text" id="review-text"
+                  placeholder="Review text"></textarea>
+            <div className="add-review__submit">
+              <button className="add-review__btn" type="submit">Post</button>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default AddReview
