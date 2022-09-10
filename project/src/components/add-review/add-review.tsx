5fb6769db50422d9da5911e381f8b25802feae9d
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-09-10 23:32:07 +0300

    Refactoring

--- project/src/components/add-review/add-review.tsx
+++ project/src/components/add-review/add-review.tsx
@@ -4,7 +4,7 @@ type AddReviewProps = {
   films: Films
 }
 
-function AddReview(props:AddReviewProps): JSX.Element {
+function AddReview(props: AddReviewProps): JSX.Element {
   const {films} = props;
   // const {rating, src} = films;
   return (
@@ -27,32 +27,45 @@ function AddReview(props:AddReviewProps): JSX.Element {
         <form action="#" className="add-review__form">
           <div className="rating">
             <div className="rating__stars">
-              <input
-                className="rating__input"
-                id={`star-${films.rating}`}
-                type="radio"
-                name="rating"
-                value={films.rating}
-                // disabled={isRadioDisabled}
-              />
-              <label className="rating__label" htmlFor={`star-${films.rating}`}>Rating {films.rating}</label>
+              {/*onChange={onRatingChange}>*/}
+              {Array.from(Array(5)).map((_, index) => {
+                const rating = index + 1;
+                return (
+                  <>
+                    <input
+                      className="rating__input"
+                      id={`star-${films.rating}`}
+                      type="radio"
+                      name="rating"
+                      value={films.rating}
+                      // disabled={isRadioDisabled}
+                    />
+                    <label className="rating__label" htmlFor={`star-${films.rating}`}>Rating {films.rating}</label>
+
+                  </>
+                )}
+                )
+              }
+              )
+
             </div>
           </div>
 
-          <div className="add-review__text">
-        <textarea className="add-review__textarea" name="review-text" id="review-text"
-                  placeholder="Review text">
 
-        </textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
+                <div className="add-review__text">
+                    <textarea className="add-review__textarea" name="review-text" id="review-text"
+                    placeholder="Review text">
 
-          </div>
+                    </textarea>
+                  <div className="add-review__submit">
+                    <button className="add-review__btn" type="submit">Post</button>
+                  </div>
+
+                </div>
         </form>
-      </div>
+                </div>
     </section>
   )
-}
+ }
 
-export default AddReview
+       export default AddReview
